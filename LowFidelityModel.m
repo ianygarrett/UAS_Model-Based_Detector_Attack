e970ba1b5a8a74c832edd4589a4926d862aeb60f
Ian Garrett <garawrett@gmail.com>
2019-12-03 11:54:18 -0500

    Add files via upload
    Removed old code that was commented out

--- LowFidelityModel.m
+++ LowFidelityModel.m
@@ -1,21 +1,14 @@
-%setup
-%uncomment this for redoing whole simulation
+%Uncomment for High-Fidelity data
 %SteadyTurnSimulation_pathFollowing
-%load LevelTrim2
+
 load Model_circle
 load circleTrajLPV_2
 
 tau = 1;
 yhat = 0;
 
-%x = xTrim2;
 dx = xOut(:,1);
 dx = dx(1:18);
-%dx(10:12) = xyzRef(1);
-%set UAS initial X/Y/H states
-%x(10)=250; x(11)=50; x(12)=100;
-
-%w = [wind(5:7,1);rand(10,1)];
 y = ones(10,1); 
 du = uOut(1);
 
@@ -50,23 +43,14 @@ else %TODO: Find actual number for sr
     runs = 34;
 end
 
-%%%%%%%%%%%%%%%%%%%%%
-%Constant increase attack
-%%%%%%%%%%%%%%%%%%%%%%
 for i = inc:inc:runs     
     k = k+4;
     ind = ind+1;
-    %Should be no wind since the low-fidelity model shouldn't have a sensor
-    %that can measure that, otherwise we'll also need measurement noise for
-    %the wind sensor
-%     w = [wind(:,k);0.996237268675862;0.632231597528220;0.136461602425878;0.923200377758063;0.947423343152599;0.342104112432378];%rand(6,1)];
-%     w = [wind(:,k);zeros(6,1)];
     du = uOut(:,k);
     dx = xOut(:,k);
     dy = yOut(:,k);
     dx = dx(1:18);
     dy = dy(1:18);
-   %dxdot = A*dx + B1*w + B2*du;
     dxdot = A*dx + B2*du;
     dydot    = C2*dx + D22*du;
     dydot = [dydot(1:3); zeros(3,1);dydot(5:10);zeros(6,1)];
@@ -77,10 +61,7 @@ for i = inc:inc:runs
   
     %Attack Height State
     dxdot(12) = dxdot(12)+50;
-  
     dxdotarray = [dxdotarray,dxdot];
-    
-
     
    % residual = abs(xplot(k+1)-dxdot(10));
    dx =xOut(:,k+1);
